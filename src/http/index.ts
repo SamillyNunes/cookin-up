40ec0fd22a807347de36f138343105da4a6cc5d6
Samilly <samillynunes19@gmail.com>
2024-12-30 08:11:26 -0300

    chore: refactoring http service

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,22 +1,20 @@
 import type ICategory from "@/interfaces/ICategory";
 import type IReceipt from "@/interfaces/IReceipt";
 
+async function getURLData<T>(url: string){
+    const response = await fetch(url);
+
+    return response.json() as T;
+}
+
 export async function getCategories(){
-    const resp = await fetch(
+    return getURLData<ICategory[]>(
         "https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json",
     );
-
-    const categories: ICategory[] = await resp.json();
-
-    return categories;
 }
 
 export async function getReceipts(){
-    const resp = await fetch(
+    return getURLData<IReceipt[]>(
         "https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json",
     );
-
-    const receipts: IReceipt[] = await resp.json();
-
-    return receipts;
 }
